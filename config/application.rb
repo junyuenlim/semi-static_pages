65d295cbc3836bb97abf5f328842ff8e10a966df
Jun Lim <junyuen@gmail.com>
2014-06-19 17:30:14 +0800

    Disable Inherited Resources

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module SemiStatic
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.app_generators.scaffold_controller = :scaffold_controller
   end
 end
