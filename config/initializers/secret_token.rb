95cea00be2c5b8ed98c45a6f758735bd261b844f
Jun Lim <junyuen@gmail.com>
2014-06-19 17:06:10 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SemiStatic::Application.config.secret_token = 'd390d2e9941e9cfe876bc66cda5b8614183d257837ef292045299b2e3da5a996eeb7ed5ea128af93db09063ca51b9e31bef187ba7496b32918fd395adc05bfd5'
